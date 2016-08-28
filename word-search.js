8295387e5c811c2cd2f1e8daa75d77126a6658c8
Vaughn Walters <vaughnwalters@gmail.com>
2016-08-28 14:45:52 -0500

    mvp criteria met

--- word-search.js
+++ word-search.js
@@ -10,21 +10,20 @@ let [,, arg] = process.argv;
 // searchterm undefined
 if (arg === undefined) {
   process.stdout.write("Usage: wordsearch.js [searchterm]\n")
-}
-else{
+} else{
   arg = arg.toLowerCase()
   readStream
   //connect streams together with `pipe`
   //split stream to break on newlines
   .pipe(es.split())
   .pipe(es.map(function (line, cb) {
-//turn this async function into a stream
+    //turn this async function into a stream
     let word = line.toString()+"\n"
     let wordLower = line.toString().toLowerCase()+"\n"
     if (wordLower.startsWith(arg)) { 
       cb(null, word)
     } else {
-// throw out data if it doesn't meet the criteria
+      // throw out data if it doesn't meet the criteria
       cb();
     } 
   }))
