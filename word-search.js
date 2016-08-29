7541efca2dc6a390dc85aa145728ac4ec8cfd3d9
Vaughn Walters <vaughnwalters@gmail.com>
2016-08-28 20:07:20 -0500

    deleted commented out code

--- word-search.js
+++ word-search.js
@@ -31,39 +31,3 @@ if (arg === undefined) {
   }))
   .pipe(transformStream).pipe(process.stdout)          
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// highWaterMark changes the amount of characters in each chunk
-// const readStream = createReadStream("/usr/share/dict/words", {highWaterMark: 10})
-
-
-// readStream.on("data", buffer => {
-//   readStream.pause();
-//   process.stdout.write(buffer.toString())
-//   // readStream.pipe(process.stdout)
-
-// });
-// // amount of time between each new character being put in stream
-// const timer = setInterval(() => readStream.resume(), 0)
-
-// readStream.on("end", () => {
-//   clearInterval(timer)
-// });
-
-
